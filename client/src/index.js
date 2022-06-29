a20226a28a433655600e3d40d6e3c53e2595fd92
Alexis Reis <79992897+alexisreis@users.noreply.github.com>
2022-06-29 16:14:50 +0200

    Sense choice widget v0.3 - JSON object in value (depends on the selected sense)

--- client/src/index.js
+++ client/src/index.js
@@ -45,14 +45,27 @@ const MySenseWidget = props => {
 		{name: 'Toucher', color: 'yellow', icon: '🤚'},
 		{name: 'Gout', color: 'pink', icon: '👅'}];
 
+	const senseObject = (senseName) => {
+		if(senseName === 'Son'){
+			return {sense: 'Son', type: '', volume: 0}
+		} else if (senseName === 'Vue'){
+			return {sense: 'Vue'}
+		}else if (senseName === 'Odeur'){
+			return {sense: 'Odeur'}
+		}else if (senseName === 'Toucher'){
+			return {sense: 'Toucher'}
+		}else if (senseName === 'Gout'){
+			return {sense: 'Gout'}
+		}
+	}
+
 	return (
 		<div className="sense-widget">
 			<div className="sense-widget-choice">
 				{senses.map(sense =>
 					<div 	key={sense.name}
-							className={currentSense?.value === sense.name ? 'sense-div selected' : 'sense-div'}
-							// style={{backgroundColor: sense.color}}
-							onClick={setSenseBody(sense.name)}>
+							className={currentSense?.value.sense === sense.name ? 'sense-div selected' : 'sense-div'}
+							onClick={setSenseBody(senseObject(sense.name))}>
 						{sense.icon}
 						<br/>
 						{sense.name}
@@ -60,27 +73,27 @@ const MySenseWidget = props => {
 				)}
 			</div>
 
-			{currentSense?.value === 'Son' ?
+			{currentSense?.value.sense === 'Son' ?
 				<div>
 					<h1>SON</h1>
 				</div> : null}
 
-			{currentSense?.value === 'Vue' ?
+			{currentSense?.value.sense === 'Vue' ?
 				<div>
 					<h1>VUE</h1>
 				</div> : null}
 
-			{currentSense?.value === 'Odeur' ?
+			{currentSense?.value.sense === 'Odeur' ?
 				<div>
 					<h1>ODEUR</h1>
 				</div> : null}
 
-			{currentSense?.value === 'Toucher' ?
+			{currentSense?.value.sense === 'Toucher' ?
 				<div>
 					<h1>TOUCHER</h1>
 				</div> : null}
 
-			{currentSense?.value === 'Gout' ?
+			{currentSense?.value.sense === 'Gout' ?
 				<div>
 					<h1>GOUT</h1>
 				</div> : null}
